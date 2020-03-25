8a25bebc9fec7dfb94ebf061973f96a12af12678
Bala Luguja <bluguja@gmail.com>
2020-03-25 19:57:00 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+
+#numbers = [1,2,3]
+#new_numbers = [9,10,16,25]
+
+
 def square_array(array)
-  # your code here
+  arr = []
+  array.each { |number| arr << number ** 2 }
+  arr
 end
+my_arr = [1, 2, 3]
+puts square_array(my_arr)
