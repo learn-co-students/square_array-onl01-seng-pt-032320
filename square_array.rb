48b438769a21fc45d104218de4351fa659824e9a
Dina Kendle <dina.kendle@gmail.com>
2020-03-30 22:28:12 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(numbers)
   new_numbers = Array.new()
-  new_numbers << numbers.each do "#{numbers} ** 2" 
+  numbers.each do |number| 
+    new_numbers << number ** 2
   end
+  new_numbers
 end
+
+
+  
